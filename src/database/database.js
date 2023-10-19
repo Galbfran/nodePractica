c78ab689fa477b44cf7b6f59d6ea84fc0d5c4b53
Galbfran <francogalbiati984@gmail.com>
2023-10-19 16:05:20 -0300

    subo configuracion inicio app con BD

--- src/database/database.js
+++ src/database/database.js
@@ -1,6 +1,13 @@
 import Sequelize from 'sequelize'
+import dotenv from 'dotenv'
 
-const sequelize = new Sequelize('nodePrueba', 'postgres', 'Franco123', {
+dotenv.config()
+
+const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME } = process.env
+
+
+export const sequelize = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
     host: 'localhost',
     dialect:'postgres' /* one of 'mysql' | 'postgres' | 'sqlite' | 'mariadb' | 'mssql' | 'db2' | 'snowflake' | 'oracle' */
   });
+
--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
 import app from "./app.js"
+import { sequelize } from "./database/database.js"
 
+async function main(){
+    try{
+        await sequelize.authenticate()
+        console.log("Conexión exitosa")
+        app.listen(3000)
+    }catch(error){
+        console.log("Error de conexión", error)
+    }
+}
 
-app.listen(3000, () => {
-    console.log("Server on port 3000")
-})
+main();
